853ac958d021d75c8d83df21ee5b1a658b107a86
luigidibella <lykos.omega@gmail.com>
2024-03-05 22:19:05 +0100

    [edit] pari e dispari

--- js/script.js
+++ js/script.js
@@ -1,27 +1,24 @@
-function isPalindrome(word) {
-  // Rimuove spazi e punteggiatura e trasforma tutto in minuscolo per evitare problemi di case sensitivity
-  const cleanWord = word.toLowerCase().replace(/[\W_]/g, '');
-  // Confronta la parola con la sua inversa
-  return cleanWord === cleanWord.split('').reverse().join('');
+const parola = prompt('Inserisci una parola');
+const parolaGirata = giraParola(parola);
+console.log(parolaGirata);
+
+if(isPalindrome(parola, parolaGirata)){
+  console.log("La parola inserita è palindroma.");
+  alert("La parola inserita è palindroma.");
+}else{
+  console.log("La parola inserita non è palindroma.");
+  alert("La parola inserita non è palindroma.");
 }
 
-// Funzione per chiedere all'utente di inserire una parola
-function checkPalindrome() {
-  const userInput = prompt("Inserisci una parola:");
-  console.log(userInput);
-  if (userInput) {
-      if (isPalindrome(userInput)) {
-          console.log("La parola inserita è un palindromo.");
-          alert("La parola inserita è un palindromo.");
-      } else {
-          console.log("La parola inserita non è un palindromo.");
-          alert("La parola inserita non è un palindromo.");
-      }
-  } else {
-      console.log("Input non valido.");
-      alert("Input non valido.");
+function giraParola(parolaDaGirare){
+  let parolaAlContrario = '';
+
+  for(let i = parolaDaGirare.length - 1; i >= 0; i--){
+    parolaAlContrario += parolaDaGirare[i];
   }
+  return parolaAlContrario;
 }
 
-// Richiama la funzione per chiedere all'utente di inserire una parola e verifica se è un palindromo
-checkPalindrome();
+function isPalindrome(parolaOriginale, parolaRigirata){
+  return parolaOriginale.toLowerCase().replace(/[\W_]/g, '') === parolaRigirata.toLowerCase().replace(/[\W_]/g, '');
+}
